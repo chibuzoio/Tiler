c29593d94c4c5b1978307305c6071f36bf2c181c
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-06-17 15:27:24 +0100

    Preparation stage of the project

--- app/src/main/java/com/chibuzo/tiler/MainActivity.kt
+++ app/src/main/java/com/chibuzo/tiler/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.chibuzo.tiler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/chibuzo/tiler/ExampleUnitTest.kt
+++ app/src/test/java/com/chibuzo/tiler/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.chibuzo.tiler
 
 import org.junit.Test
 
