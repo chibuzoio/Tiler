7cce82d995e14a90d3246f59dd2e5d63888b835c
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-06-18 00:39:52 +0100

    Resolved window preview issue on application start up

--- app/src/main/java/com/chibuzo/tiler/MainActivity.kt
+++ app/src/main/java/com/chibuzo/tiler/MainActivity.kt
@@ -2,11 +2,18 @@ package com.chibuzo.tiler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+            setContentView(R.layout.activity_main)
+        }, 2000)
     }
 }
 
