2398d86bc14f4a088717a6c56108b426411f4e8d
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-06-18 14:43:17 +0100

    Begining of the homepage

--- app/src/main/java/com/chibuzo/tiler/MainActivity.kt
+++ app/src/main/java/com/chibuzo/tiler/MainActivity.kt
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.chibuzo.tiler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
         Handler(Looper.getMainLooper()).postDelayed(
             {
-            setContentView(R.layout.activity_main)
-        }, 2000)
+                binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                val includedHeader = binding.includedHeader
+
+                binding.tileCalculator.setOnClickListener {
+
+                }
+
+                binding.nigerianTiles.setOnClickListener{
+
+                }
+            }, 2000)
     }
 }
 
