472492d41a0016fcca12c7c6d5467027b99c8ec4
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-06-19 05:32:33 +0100

    New activities and resource files added to the project

--- app/src/main/java/com/chibuzo/tiler/activity/TileCalculatorActivity.kt
+++ app/src/main/java/com/chibuzo/tiler/activity/TileCalculatorActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chibuzo.tiler.R
 import com.chibuzo.tiler.databinding.ActivityMainBinding
+import com.chibuzo.tiler.databinding.ActivityTileCalculatorBinding
 
 class TileCalculatorActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityTileCalculatorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tile_calculator)
+
+        binding = ActivityTileCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cartonSizeMenu.genericMenuText.text = "Tile Carton Size";
+        binding.totalSquareMeterMenu.genericMenuText.text = "Tile Total Square Meter";
+        binding.totalPriceMenu.genericMenuText.text = "Tile Total Price";
+        binding.squareMeterMenu.genericMenuText.text = "Tile Square Meter";
+        binding.packingSizeMenu.genericMenuText.text = "Tile Packing Size";
     }
 }
 
