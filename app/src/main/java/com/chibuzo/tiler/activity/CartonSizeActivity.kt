7de341d4fdc85d3c68a143406738d9552085129a
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-06-20 02:50:56 +0100

    Started the implementation of all the calculators

--- app/src/main/java/com/chibuzo/tiler/activity/CartonSizeActivity.kt
+++ app/src/main/java/com/chibuzo/tiler/activity/CartonSizeActivity.kt
@@ -3,6 +3,8 @@ package com.chibuzo.tiler.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chibuzo.tiler.databinding.ActivityCartonSizeBinding
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
 class CartonSizeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCartonSizeBinding
@@ -16,7 +18,28 @@ class CartonSizeActivity : AppCompatActivity() {
         binding.cartonSizeCalculator.genericHeaderText.text = "Total Cartons And Pieces Calculator";
         binding.totalSquareMeterInput.genericInputLabel.text = "Total Square Meter";
         binding.cartonSquareMeterInput.genericInputLabel.text = "Square Meter Per Carton";
+        binding.numberOfPiecesInput.genericInputLabel.text = "Number Of Pieces Per Carton";
         binding.calculateCartonSize.genericButtonLabel.text = "Calculate";
+
+        val totalSquareMeter = binding.totalSquareMeterInput.genericInputEditor.text
+        val cartonSquareMeter = binding.cartonSquareMeterInput.genericInputEditor.text
+        val numberOfPieces = binding.numberOfPiecesInput.genericInputEditor.text
+
+        binding.calculateCartonSize.genericButtonLayout.setOnClickListener{
+            if (totalSquareMeter.isNotBlank() &&
+                cartonSquareMeter.isNotBlank() && numberOfPieces.isNotBlank()) {
+                val numberOfPiecesValue = numberOfPieces.toString().toInt()
+                val totalSquareMeterValue = totalSquareMeter.toString().toDouble()
+                val cartonSquareMeterValue = cartonSquareMeter.toString().toDouble()
+
+                val cartonSizeFullResult = totalSquareMeterValue.div(cartonSquareMeterValue)
+                val cartonSizeResult = (floor(cartonSizeFullResult)).roundToInt()
+                val remainingPieces = ((cartonSizeFullResult % 1) * numberOfPiecesValue).roundToInt()
+
+                binding.calculateCartonResult.text =
+                    "$cartonSizeResult cartons and $remainingPieces pieces!";
+            }
+        }
     }
 }
 
