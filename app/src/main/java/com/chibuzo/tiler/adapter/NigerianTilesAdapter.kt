cf4567aabd073524fb59eb8ea49658ce89b816ca
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-06-23 03:31:54 +0100

    Begun the implementation of Nigerian Tiles activity

---
+++ app/src/main/java/com/chibuzo/tiler/adapter/NigerianTilesAdapter.kt
@@ -0,0 +1,14 @@
+package com.chibuzo.tiler.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.chibuzo.tiler.model.NigerianTilesModel
+
+class NigerianTilesAdapter(val nigerianTiles: ArrayList<NigerianTilesModel>) : RecyclerView.ViewHolder<NigerianTilesAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
+
+
---
+++ app/src/main/java/com/chibuzo/tiler/model/NigerianTilesModel.kt
@@ -0,0 +1,8 @@
+package com.chibuzo.tiler.model
+
+data class NigerianTilesModel(val imageName: String,
+                              val dimension: String,
+                              val packingSize: String,
+                              val squareMeter: String)
+
+
