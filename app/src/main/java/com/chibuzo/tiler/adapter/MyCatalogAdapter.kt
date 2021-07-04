0b732192fe72755893286ea1f3aaa045a36276ea
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2021-07-04 14:05:23 +0100

    About to complete the development of uploading, editing, deleting and viewing of custom catalog tiles

--- app/src/main/java/com/chibuzo/tiler/adapter/MyCatalogAdapter.kt
+++ app/src/main/java/com/chibuzo/tiler/adapter/MyCatalogAdapter.kt
@@ -2,19 +2,14 @@ package com.chibuzo.tiler.adapter
 
 import android.content.Context
 import android.content.ContextWrapper
-import android.graphics.BitmapFactory
-import android.util.Base64
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import com.chibuzo.tiler.R
 import com.chibuzo.tiler.databinding.ActivityMyCatalogBinding
 import com.chibuzo.tiler.databinding.RecyclerMyCatalogBinding
@@ -32,12 +27,6 @@ class MyCatalogAdapter(private val myCatalogTiles: ArrayList<MyCatalogTilesModel
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        if ((itemCount - position) < 2) {
-            val params = holder.binding.myCatalogTileLayout.layoutParams as ViewGroup.MarginLayoutParams
-            params.bottomMargin = 250
-            holder.binding.myCatalogTileLayout.layoutParams = params
-        }
-
         val contextWrapper = ContextWrapper(holder.itemView.context)
         val directory = contextWrapper.getDir("imageDir", Context.MODE_PRIVATE)
         val file = File(directory, myCatalogTiles[position].tileImageName)
